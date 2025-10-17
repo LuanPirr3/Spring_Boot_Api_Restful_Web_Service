855e8e18f12ff671b780b9817232e1cdcf71083d
LuanPirr3 <lu4n.engs0ft@gmail.com>
2025-10-17 18:40:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring_Boot_Api_Restful_Web_Service
---
+++ src/test/java/com/api_restful_web_service/spring_boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_restful_web_service.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
