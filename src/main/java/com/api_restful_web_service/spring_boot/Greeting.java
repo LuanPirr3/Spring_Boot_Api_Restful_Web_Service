3a1272ebc7b85276c7bad19e6604e7e29929756e
LuanPirr3 <lu4n.engs0ft@gmail.com>
2025-10-19 19:56:44 -0300

    📝 finishing settings server ✅

---
+++ src/main/java/com/api_restful_web_service/spring_boot/Greeting.java
@@ -0,0 +1,4 @@
+package com.api_restful_web_service.spring_boot;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/main/java/com/api_restful_web_service/spring_boot/RestServiceAplication.java
@@ -0,0 +1,9 @@
+package com.api_restful_web_service.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+
+public class RestServiceAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceAplication.class, args);
+    }
+}
